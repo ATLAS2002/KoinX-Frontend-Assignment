657305003517d485e791ce57612670530d2d2b07
atlas <siddharth211102@gmail.com>
2024-03-08 09:06:33 +0530

    chore: changed favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.coingecko.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
